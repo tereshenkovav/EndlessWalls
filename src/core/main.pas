43ca6915104f291bf6942c45ba5d175ee27b8a61
TAV <tereshenkov@mail.ru>
2024-09-11 12:18:57 +0300

    Включен вывод FPS в окне игры при наличиие отладочного файла developer.config

--- src/core/main.pas
+++ src/core/main.pas
@@ -20,6 +20,7 @@ begin
   ChDir(ExtractFilePath(ParamStr(0))+PATH_SEP+'..'+PATH_SEP+'data') ;
   game:=TGame.Create(1024,768,'EndlessWalls',
     'EndlessWalls','images'+PATH_SEP+'icon.png') ;
+  if FileExists('developer.config') then game.enableFPSInTitle(True) ;
   game.Run(TSceneGame.Create()) ;
   game.Free ;
 end ;
